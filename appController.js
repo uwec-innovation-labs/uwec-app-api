f375a9fc21e4aebc2279a9da9eded093ea00d96c
Sullivan Prellwitz <prellwsr8859@uwec.edu>
2019-06-05 16:06:18 -0500

    Added more laundry room data

--- appController.js
+++ appController.js
@@ -3,18 +3,19 @@ const request = require('request-promise')
 const axios = require('axios')
 const cheerio = require('cheerio')
 const newsURL = 'https://www.spectatornews.com/'
-const foodURL = 'https://www.uwec.edu/campus-life/housing-dining/dining/food-services/dining-hours/'
+const foodURL =
+  'https://www.uwec.edu/campus-life/housing-dining/dining/food-services/dining-hours/'
 const calendarURL = 'https://calendar.uwec.edu/MasterCalendar.aspx'
 
 const diningURL = 'https://bite-external-api.azure-api.net/extern/menus/'
-const hilltopLocationID = "84956001";
-const hilltopMenuID = "14916";
-const daviesLocationID = "84956004";
-const daviesMenuID = "24TW3";
-const dulanyLocationID = "84956010";
-const dulanyMenuID = "14843";
-const cabinLocationID = "84956022";
-const cabinMenuID = "14844";
+const hilltopLocationID = '84956001'
+const hilltopMenuID = '14916'
+const daviesLocationID = '84956004'
+const daviesMenuID = '24TW3'
+const dulanyLocationID = '84956010'
+const dulanyMenuID = '14843'
+const cabinLocationID = '84956022'
+const cabinMenuID = '14844'
 
 /*async function getEvents(parent, args, context, info) {
   setTimeout( function (i) {
@@ -65,12 +66,47 @@ async function getLaundry(parent, args, context, info) {
 // Function to grab laundry room data - dummy data
 async function getLaundryRoom(parent, args, context, info) {
   // Return laundryRoom with dummy data for the stats of the room
-  var laundryRoom = {
-    id: parent.building,
-    totalNumWashers: 9,
-    totalNumDryers: 9,
-    washersAvailable: 4,
-    dryersAvailable: 5
+  let laundryRoom
+  if (parent === 'towers') {
+    laundryRoom = {
+      id: parent.building,
+      totalNumWashers: 9,
+      totalNumDryers: 9,
+      washersAvailable: 4,
+      dryersAvailable: 5
+    }
+  } else if (parent === 'bridgman') {
+    laundryRoom = {
+      id: parent.building,
+      totalNumWashers: 4,
+      totalNumDryers: 4,
+      washersAvailable: 4,
+      dryersAvailable: 5
+    }
+  } else if (parent === 'horan') {
+    laundryRoom = {
+      id: parent.building,
+      totalNumWashers: 22,
+      totalNumDryers: 22,
+      washersAvailable: 4,
+      dryersAvailable: 5
+    }
+  } else if (parent === 'oak') {
+    laundryRoom = {
+      id: parent.building,
+      totalNumWashers: 1,
+      totalNumDryers: 2,
+      washersAvailable: 4,
+      dryersAvailable: 5
+    }
+  } else {
+    laundryRoom = {
+      id: parent.building,
+      totalNumWashers: 999,
+      totalNumDryers: 999,
+      washersAvailable: 4,
+      dryersAvailable: 5
+    }
   }
   return laundryRoom
 }
@@ -227,189 +263,248 @@ async function getNews(parent, args, context, info) {
   return data
 }
 
-
 function GetFormattedDate() {
-  var todayTime = new Date();
-  var month = todayTime.getMonth() + 1;
-  var day = todayTime.getDate();
-  return month + "-" + day;
+  var todayTime = new Date()
+  var month = todayTime.getMonth() + 1
+  var day = todayTime.getDate()
+  return month + '-' + day
 }
 
 async function getDining(parent, args, context, info) {
-  var today = GetFormattedDate();
-  var REastBreakfast = [];
-  var REastLunch = [];
-  var REastDinner = [];
+  var today = GetFormattedDate()
+  var REastBreakfast = []
+  var REastLunch = []
+  var REastDinner = []
   await axios({
     method: 'get',
-    url: diningURL + hilltopLocationID + "/" + today + "/" + today + "/" + hilltopMenuID,
+    url:
+      diningURL +
+      hilltopLocationID +
+      '/' +
+      today +
+      '/' +
+      today +
+      '/' +
+      hilltopMenuID,
     headers: {
-      "sodexo-accesscodes": "24TW3",
-      "Ocp-Apim-Subscription-Key": "e2e4286402cd4195ad6f8ac90ebdf9d4",
+      'sodexo-accesscodes': '24TW3',
+      'Ocp-Apim-Subscription-Key': 'e2e4286402cd4195ad6f8ac90ebdf9d4'
     }
-  })
-  .then(response => {
+  }).then(response => {
     if (response.status === 200) {
-      var menuDays = response.data[0].menuDays;
-      var menuDayOne = menuDays[0].menuItems;
-      menuDayOne.forEach((menuItem) => {
-        if (menuItem.course == "Main Line") {
-          if (menuItem.meal === "Breakfast") {
-            REastBreakfast.push(menuItem.formalName);
-          } else if (menuItem.meal === "Lunch") {
-            REastLunch.push(menuItem.formalName);
-          } else if (menuItem.meal === "Dinner") {
-            REastDinner.push(menuItem.formalName);
+      var menuDays = response.data[0].menuDays
+      var menuDayOne = menuDays[0].menuItems
+      menuDayOne.forEach(menuItem => {
+        if (menuItem.course == 'Main Line') {
+          if (menuItem.meal === 'Breakfast') {
+            REastBreakfast.push(menuItem.formalName)
+          } else if (menuItem.meal === 'Lunch') {
+            REastLunch.push(menuItem.formalName)
+          } else if (menuItem.meal === 'Dinner') {
+            REastDinner.push(menuItem.formalName)
           }
         }
-      });
+      })
     }
-  });
+  })
 
-  var bluFlameMenu = [];
-  var marketMenu = [];
-  var monGrillMenu = [];
-  var tresHabMenu = [];
+  var bluFlameMenu = []
+  var marketMenu = []
+  var monGrillMenu = []
+  var tresHabMenu = []
 
   await axios({
     method: 'get',
-    url: diningURL + daviesLocationID + "/" + today + "/" + today + "/" + daviesMenuID,
+    url:
+      diningURL +
+      daviesLocationID +
+      '/' +
+      today +
+      '/' +
+      today +
+      '/' +
+      daviesMenuID,
     headers: {
-      "sodexo-accesscodes": "24TW3",
-      "Ocp-Apim-Subscription-Key": "e2e4286402cd4195ad6f8ac90ebdf9d4",
+      'sodexo-accesscodes': '24TW3',
+      'Ocp-Apim-Subscription-Key': 'e2e4286402cd4195ad6f8ac90ebdf9d4'
     }
-  })
-  .then(response => {
+  }).then(response => {
     if (response.status === 200) {
-      menuItems = response.data[0].menuDays[0].menuItems;
+      menuItems = response.data[0].menuDays[0].menuItems
       menuItems.forEach(function(menuItem) {
-        if (menuItem.course === "Mongolian Noodle Bowl" || menuItem.course === "Mongolian Rice Bowl") {
-          if (menuItem.planningGroupDescription !== "MISC. ITEMS") {
-            monGrillMenu.push(menuItem.formalName);
+        if (
+          menuItem.course === 'Mongolian Noodle Bowl' ||
+          menuItem.course === 'Mongolian Rice Bowl'
+        ) {
+          if (menuItem.planningGroupDescription !== 'MISC. ITEMS') {
+            monGrillMenu.push(menuItem.formalName)
           }
-        } else if (menuItem.course === "Tres Habeneros") {
-          if (menuItem.uomDescription === "1 ENTREE") {
-            tresHabMenu.push(menuItem.formalName);
+        } else if (menuItem.course === 'Tres Habeneros') {
+          if (menuItem.uomDescription === '1 ENTREE') {
+            tresHabMenu.push(menuItem.formalName)
           }
-        } else if (menuItem.course === "Blu Flame Grill") {
-          if (menuItem.planningGroupDescription === "SAND-POULTRY (HOT)") {
-            bluFlameMenu.push(menuItem.formalName);
+        } else if (menuItem.course === 'Blu Flame Grill') {
+          if (menuItem.planningGroupDescription === 'SAND-POULTRY (HOT)') {
+            bluFlameMenu.push(menuItem.formalName)
           }
-        } else if (menuItem.course === "Blu Flame Breakfast") {
-          if (menuItem.foodMainCategoryDescription === "Mains") {
-            bluFlameMenu.push("(Breakfast):" + menuItem.formalName);
+        } else if (menuItem.course === 'Blu Flame Breakfast') {
+          if (menuItem.foodMainCategoryDescription === 'Mains') {
+            bluFlameMenu.push('(Breakfast):' + menuItem.formalName)
           }
         } else {
-          if (menuItem.course !== "Blu Flame Homestyle Bar" && menuItem.course !== "Seasons Salad Bar" && menuItem.planningGroupDescription !== "SNACKS" && menuItem.planningGroupDescription !== "MISC. ITEMS" && menuItem.planningGroupDescription !== "Other") {
-            marketMenu.push(menuItem.formalName);
+          if (
+            menuItem.course !== 'Blu Flame Homestyle Bar' &&
+            menuItem.course !== 'Seasons Salad Bar' &&
+            menuItem.planningGroupDescription !== 'SNACKS' &&
+            menuItem.planningGroupDescription !== 'MISC. ITEMS' &&
+            menuItem.planningGroupDescription !== 'Other'
+          ) {
+            marketMenu.push(menuItem.formalName)
           }
         }
-      });
+      })
     }
-  });
-
-  var dulanyMenu = [];
-  await axios({
-  method: 'get',
-  url: diningURL + dulanyLocationID + "/" + today + "/" + today + "/" + dulanyMenuID,
-  headers: {
-    "sodexo-accesscodes": "24TW3",
-    "Ocp-Apim-Subscription-Key": "e2e4286402cd4195ad6f8ac90ebdf9d4",
-  }
   })
-  .then(response => {
-    if (response.status === 200) {
-      var menuItems = response.data[0].menuDays[0].menuItems;
-      menuItems.forEach(function(menuItem) {
-        dulanyMenu.push(menuItem.formalName);
-      });
+
+  var dulanyMenu = []
+  await axios({
+    method: 'get',
+    url:
+      diningURL +
+      dulanyLocationID +
+      '/' +
+      today +
+      '/' +
+      today +
+      '/' +
+      dulanyMenuID,
+    headers: {
+      'sodexo-accesscodes': '24TW3',
+      'Ocp-Apim-Subscription-Key': 'e2e4286402cd4195ad6f8ac90ebdf9d4'
     }
-  });
+  }).then(response => {
+    if (response.status === 200) {
+      var menuItems = response.data[0].menuDays[0].menuItems
+      menuItems.forEach(function(menuItem) {
+        dulanyMenu.push(menuItem.formalName)
+      })
+    }
+  })
 
   let data = await axios
-  .get(foodURL)
-  .then(response => {
-    if (response.status === 200) {
-      const html = response.data
-      const $ = cheerio.load(html)
-      let dining = [];
-      var diningIndex = -1;
+    .get(foodURL)
+    .then(response => {
+      if (response.status === 200) {
+        const html = response.data
+        const $ = cheerio.load(html)
+        let dining = []
+        var diningIndex = -1
 
-      var diningTables = ["section[id=sec-table-s]", "section[id=sec-table-s-5]"];
-      diningTables.forEach(function(table) {
-        $(table).children('table').children('tbody').children().each((i, elm) => {
-          $(elm).children().each((j, elm2) => {
-            var rawDiningName = $(elm2).text();
-            if ((rawDiningName !== "") && (rawDiningName !== "Riverview Cafe East") && (rawDiningName !== "Riverview Cafe")) {
-              if (j == 0) {
-                var diningName = rawDiningName.trim().replace(/\xa0/g, "").replace(/ +/g, " ");
-                diningIndex++;
-                if (diningName === "Breakfast") {
-                  diningName = "Riverview Cafe East - Breakfast";
-                  dining[diningIndex] = {
-                    menu: REastBreakfast
+        var diningTables = [
+          'section[id=sec-table-s]',
+          'section[id=sec-table-s-5]'
+        ]
+        diningTables.forEach(function(table) {
+          $(table)
+            .children('table')
+            .children('tbody')
+            .children()
+            .each((i, elm) => {
+              $(elm)
+                .children()
+                .each((j, elm2) => {
+                  var rawDiningName = $(elm2).text()
+                  if (
+                    rawDiningName !== '' &&
+                    rawDiningName !== 'Riverview Cafe East' &&
+                    rawDiningName !== 'Riverview Cafe'
+                  ) {
+                    if (j == 0) {
+                      var diningName = rawDiningName
+                        .trim()
+                        .replace(/\xa0/g, '')
+                        .replace(/ +/g, ' ')
+                      diningIndex++
+                      if (diningName === 'Breakfast') {
+                        diningName = 'Riverview Cafe East - Breakfast'
+                        dining[diningIndex] = {
+                          menu: REastBreakfast
+                        }
+                      } else if (diningName === 'Lunch') {
+                        diningName = 'Riverview Cafe East - Lunch'
+                        dining[diningIndex] = {
+                          menu: REastLunch
+                        }
+                      } else if (diningName === 'Dinner') {
+                        diningName = 'Riverview Cafe East - Dinner'
+                        dining[diningIndex] = {
+                          menu: REastDinner
+                        }
+                      } else if (diningName === 'Blu Flame Grill') {
+                        dining[diningIndex] = {
+                          menu: bluFlameMenu
+                        }
+                      } else if (diningName === 'Tres Habeneros') {
+                        dining[diningIndex] = {
+                          menu: tresHabMenu
+                        }
+                      } else if (diningName === 'Mongolian Grill') {
+                        dining[diningIndex] = {
+                          menu: monGrillMenu
+                        }
+                      } else if (diningName === 'Marketplace') {
+                        dining[diningIndex] = {
+                          menu: marketMenu
+                        }
+                      } else if (diningName === 'The Dulany Inn') {
+                        dining[diningIndex] = {
+                          menu: dulanyMenu
+                        }
+                      } else {
+                        dining[diningIndex] = {}
+                      }
+                      dining[diningIndex].hours = []
+                      dining[diningIndex].name = diningName
+                    } else if (j == 1) {
+                      dining[diningIndex].hours.push(
+                        'Monday: ' + $(elm2).text()
+                      )
+                    } else if (j == 2) {
+                      dining[diningIndex].hours.push(
+                        'Tuesday: ' + $(elm2).text()
+                      )
+                    } else if (j == 3) {
+                      dining[diningIndex].hours.push(
+                        'Wednesday: ' + $(elm2).text()
+                      )
+                    } else if (j == 4) {
+                      dining[diningIndex].hours.push(
+                        'Thursday: ' + $(elm2).text()
+                      )
+                    } else if (j == 5) {
+                      dining[diningIndex].hours.push(
+                        'Friday: ' + $(elm2).text()
+                      )
+                    } else if (j == 6) {
+                      dining[diningIndex].hours.push(
+                        'Saturday: ' + $(elm2).text()
+                      )
+                    } else if (j == 7) {
+                      dining[diningIndex].hours.push(
+                        'Sunday: ' + $(elm2).text()
+                      )
+                    }
                   }
-                } else if (diningName === "Lunch") {
-                  diningName = "Riverview Cafe East - Lunch";
-                  dining[diningIndex] = {
-                    menu: REastLunch
-                  }
-                } else if (diningName === "Dinner") {
-                  diningName = "Riverview Cafe East - Dinner";
-                  dining[diningIndex] = {
-                    menu: REastDinner
-                  }
-                } else if (diningName === "Blu Flame Grill") {
-                  dining[diningIndex] = {
-                    menu: bluFlameMenu
-                  }
-                } else if (diningName === "Tres Habeneros") {
-                  dining[diningIndex] = {
-                    menu: tresHabMenu
-                  }
-                } else if (diningName === "Mongolian Grill") {
-                  dining[diningIndex] = {
-                    menu: monGrillMenu
-                  }
-                } else if (diningName === "Marketplace") {
-                  dining[diningIndex] = {
-                    menu: marketMenu
-                  }
-                } else if (diningName === "The Dulany Inn") {
-                  dining[diningIndex] = {
-                    menu: dulanyMenu
-                  }
-                } else {
-                  dining[diningIndex] = {};
-                }
-                dining[diningIndex].hours = [];
-                dining[diningIndex].name = diningName;
-              } else if (j == 1) {
-                dining[diningIndex].hours.push("Monday: " + $(elm2).text());
-              } else if (j == 2) {
-                dining[diningIndex].hours.push("Tuesday: " + $(elm2).text());
-              } else if (j == 3) {
-                dining[diningIndex].hours.push("Wednesday: " + $(elm2).text());
-              } else if (j == 4) {
-                dining[diningIndex].hours.push("Thursday: " + $(elm2).text());
-              } else if (j == 5) {
-                dining[diningIndex].hours.push("Friday: " + $(elm2).text());
-              } else if (j == 6) {
-                dining[diningIndex].hours.push("Saturday: " + $(elm2).text());
-              } else if (j == 7) {
-                dining[diningIndex].hours.push("Sunday: " + $(elm2).text());
-              }
-            }
-          });
-        });
-      });
-      return dining;
-    }
-  })
-  .then(response => {
-    return response
-  });
-  return data;
+                })
+            })
+        })
+        return dining
+      }
+    })
+    .then(response => {
+      return response
+    })
+  return data
 }
 
 module.exports = {
